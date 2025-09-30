709a22c38d81c2f759c8c80ce0446e0b26bda73a
ShivendraPratap20 <shivendragkp2002@gmail.com>
2025-09-30 15:22:52 +0530

    Fix oauth route

--- backend/src/routes/OAuth.js
+++ backend/src/routes/OAuth.js
@@ -13,11 +13,10 @@ router.get("/callback",
         try {
             const token = jwt.sign({_id:req.user._id, userID:req.user.userID}, process.env.SECRET_KEY);
             res.cookie("JWT", token);
-            const userData = await userModel.find({ userID: req.user.userID });
-            res.redirect(`${process.env.CLIENT_URL}/dashboard`)
+            res.redirect(`/dashboard`)
         } catch (error) {
             console.error("Google login error:", error)
-            res.redirect(`${process.env.CLIENT_URL}/`)
+            res.redirect(`/`)
         }
     }
 );
